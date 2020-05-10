33cec53d7934f296ca133d502053801d2763cf9c
Johann Dickson <johann.dickson@gmail.com>
2020-05-10 21:21:47 +0100

    Update invocation URL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 data "aws_region" "current" {}
 
 output "endpoint" {
-  value = "https://${aws_api_gateway_rest_api.budget.id}.execute-api.${data.aws_region.current.name}.amazonaws.com${aws_api_gateway_resource.monzo.path}"
+  value = "${aws_api_gateway_stage.v1.invoke_url}${aws_api_gateway_resource.monzo.path}"
 }
